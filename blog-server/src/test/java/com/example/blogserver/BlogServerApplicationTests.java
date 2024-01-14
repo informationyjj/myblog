fad67a4fc99cffd2da8550b34fc2b01bd26ff927
yjj <2328921729@qq.com>
2024-01-15 07:36:21 +0800

    2024-1-18

--- blog-server/src/test/java/com/example/blogserver/BlogServerApplicationTests.java
+++ blog-server/src/test/java/com/example/blogserver/BlogServerApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.blogserver;
 
+import com.alibaba.nacos.api.naming.pojo.healthcheck.impl.Http;
+import com.example.blogserver.Vo.RegistedVo;
+import com.example.blogserver.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootTest
 class BlogServerApplicationTests {
 
+    @Autowired
+    HttpServletRequest httpServletRequest;
+    @Autowired
+    private UserService userService;
+
     @Test
     void contextLoads() {
+
+        RegistedVo user = new RegistedVo("2876944236@qq.com", "ะกรื", "111", "234566");
+        Boolean registed = userService.registed(user, httpServletRequest);
+        System.out.println(registed);
     }
 
 }
